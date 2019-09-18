0f95dd2fe92281f67662bb1a941bec87fa397659
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2019-09-18 11:14:59 -0300

    18.09.2019

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskI } from '../models/task.interface';
+import { TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+  todos: TaskI[];
+
+  constructor(private todoService: TodoService){}
+  
+  ngOnInit(){
+    
+    this.todoService.getTodos().subscribe(res => this.todos = res );
+
+  }
+  
+}
